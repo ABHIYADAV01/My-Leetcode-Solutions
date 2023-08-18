eaf089b5ac904e1673f9b369c7886cdb78e7a01f
Abhishek <abhishekyadav.cs21@rvce.edu.in>
2023-08-18 19:53:49 +0530

    added a code in two pointers

---
+++ Two pointers/283. Move Zeroes.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+     //using two pointers
+    void moveZeroes(vector<int>& nums) {
+
+        int i = 0;
+
+      for(int j=0;j<nums.size();j++){
+          if(nums[j]!=0){
+              nums[i++]=nums[j];
+          }
+      }
+
+      while(i<nums.size()){
+          nums[i++]=0;
+      }
+
+          
+        
+    }
+};
